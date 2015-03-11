f11b444e6d47383e460a00e97cdbdd9ffe1af395
CaroBorges <carobe18685@gmail.com>
2015-03-11 14:59:03 -0600

    agregar perfil del estudiante

--- scripts/qualify-documents.js
+++ scripts/qualify-documents.js
@@ -6,23 +6,23 @@ app.controller("dropdownQualify", function(){
 	this.students  = Query('users','rol',2);
 	this.group     = Query('groups','id',1);
 	this.documents = Query('documents','id', 1);
-	// this.selectedIndex;
+	this.selectedIndex;
 
-	// var i = 0,
-	// 	l = this.students.length;
+	var i = 0,
+		l = this.students.length;
 
-	// this.dropdown = function( $index ) {
-	// 	console.log('dp '+$index);
-	// 	if( $this.selectedIndex === $index ){
-	// 		$this.selectedIndex = -1;
-	// 	} else {
-	// 		$this.selectedIndex = $index;
-	// 	}
-	// }
-	// this.getClass = function( $index ){
-	// 	console.log('getclass '+$index);
-	// 	if( $index === $this.selectedIndex ){
-	// 		return 'active';
-	// 	}
-	// }
+	this.dropdown = function( $index ) {
+		console.log('dp '+$index);
+		if( $this.selectedIndex === $index ){
+			$this.selectedIndex = -1;
+		} else {
+			$this.selectedIndex = $index;
+		}
+	}
+	this.getClass = function( $index ){
+		console.log('getclass '+$index);
+		if( $index === $this.selectedIndex ){
+			return 'active';
+		}
+	}
 });
