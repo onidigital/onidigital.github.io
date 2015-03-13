edfa98deb928de1a8999244d743c5ab19e50cbc9
Irene Araya <iarayal>
2015-03-12 23:19:08 -0600

    el formulario de agregar estudiante con mensaje de validación

--- scripts/form-add-student.js
+++ scripts/form-add-student.js
@@ -5,18 +5,19 @@ app.controller('addStudentController', function(){
 	var $this = this;
 
 	this.careerAspects = [];
-	this.newCareerAspects = {};
+	this.newCareerAspect = {};
 	this.students = Query('users', 'rol', 2);
 	this.careers   = Query('careers','-','all');
 	this.newStudent = {};
+
 
 	console.table(this.students);
 
 	this.addNewCareerAspect = function() {
   		var newItemNo = $this.careerAspects.length+1;
-  		if ($this.newCareerAspects.name != '') {
-  		$this.careerAspects.unshift( $this.newCareerAspects );
-  		$this.newCareerAspects = {};
+  		if ($this.newCareerAspect.name != '') {
+  		$this.careerAspects.unshift( $this.newCareerAspect );
+  		$this.newCareerAspect = {};
   		};
 	};
 
@@ -24,11 +25,15 @@ app.controller('addStudentController', function(){
 		$this.careerAspects.splice(index,1);
 	};	
 
-	
 	this.addStudent = function(){
 		$this.students.push($this.newStudent);
 		$this.newStudent = {};
 		console.table($this.students);
+
+		if($this.formAddStudent.$invalid){
+			$this.showMessage = true;
+		}
 	}
+
 
 });
