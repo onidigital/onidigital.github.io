4e4b3747e1e5dbf5f10bf2afbef5cb637e3bb5b4
Irene Araya <iarayal>
2015-03-14 02:59:04 -0600

    cambios en 2 archivos para quitar headers

--- scripts/infoConsultCourses.js
+++ scripts/infoConsultCourses.js
@@ -1,5 +1,3 @@
-var app = angular.module("infoCourses", ["ui.router"]);
-
 app.controller('InfoCoursesController', function() {
 	this.courses = Query('courses'); //this hace referencia al controlador
 });
